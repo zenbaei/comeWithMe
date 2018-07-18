0c110e276705b14907cd5111690686004a93e899
zenbaei@gmail.com <islam.zenbaei@aurea.com>
2018-07-18 14:35:49 +0200

    re-link icon fonts

--- android/app/src/main/java/com/comewithme/MainApplication.java
+++ android/app/src/main/java/com/comewithme/MainApplication.java
@@ -3,6 +3,7 @@ package com.comewithme;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new FBSDKPackage(mCallbackManager)
       );
     }
